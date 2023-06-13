302281d12ab1927f98b35a159a91558aa69abc0c
Leonard Galora <Leonard.Galora.-ND@disney.com>
2023-06-13 15:28:41 +0800

    change image

--- models/library/library_books.model.js
+++ models/library/library_books.model.js
@@ -15,7 +15,7 @@ const BookSchema = new mongoose.Schema(
        
         },
         bookThumbnailImg:{
-            type:String,
+            type:Object,
         },
       
         bookThumbnailAlt:{
